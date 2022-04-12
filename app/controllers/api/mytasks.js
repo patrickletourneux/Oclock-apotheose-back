dd26aa2da4658bea15bb8fce4cfbe6e1c4b380b2
patrickletourneux <patrick.letourneux20@gmail.com>
2022-04-12 13:32:28 +0200

    mytask controller modification to respect frontend need

--- app/controllers/api/mytasks.js
+++ app/controllers/api/mytasks.js
@@ -13,9 +13,25 @@ module.exports = {
       debug('pas de user trouvé pour cet id');
       throw new ApiError('user not found', { statusCode: 404 });
     }
-    const mytasks = await mytasksDataMapper.findOneByPk(req.params.id);
-    debug(mytasks);
+    const mytasks = await mytasksDataMapper.findOneByPk(req.params.id); 
+    const newTasks = [];
 
-    return res.status(200).json(mytasks);
+    mytasks.home_task.forEach((taskHome) => {
+      const taskAttributed = mytasks.attributed_task.find((e) => e.id === taskHome.id);
+      if (taskAttributed) {
+        taskHome.attributed = true;
+      } else {
+        taskHome.attributed = false;
+      }
+      newTasks.push(taskHome);
+    });
+    const obj = {
+      // home_tasks: mytasks.home_task,
+      // attributed_tasks: mytasks.attributed_task,
+      home_tasks: newTasks,
+      done_tasks: mytasks.done_task,
+    };
+
+    return res.status(200).json(obj);
   },
 };
