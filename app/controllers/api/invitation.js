de931199105f05661ce9c77d2cf8e51cdcff5f60
patrickletourneux <patrick.letourneux20@gmail.com>
2022-04-20 16:39:31 +0200

    modif message in mail invitation

--- app/controllers/api/invitation.js
+++ app/controllers/api/invitation.js
@@ -20,7 +20,7 @@ module.exports = {
       req.body.email,
       // ne pas depasser le nombre de caractères max pour le subject
       'Cduprops invitation ',
-      `rejoins la maison ${home.id.toString()} avec le mot de passe ${home.password.toString()} `,
+      `rejoins la maison "${home.name}" avec le code "${home.password.toString()}" `,
     );
     debug('envoi message', envoi);
 
