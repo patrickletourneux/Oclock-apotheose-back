968b4a7143c8fb82eebb1744fdc969b198335f57
patrickletourneux <patrick.letourneux20@gmail.com>
2022-07-14 21:38:52 +0200

    dashboard.test.js , my first mock jest, congratulations to me

--- app/controllers/api/dashboard.test.js
+++ app/controllers/api/dashboard.test.js
@@ -1,6 +1,26 @@
 require('dotenv').config();
-const { object } = require('joi');
-const dashboardController = require('./dashboard');
+// const { object } = require('joi');
+const dashboardController  = require('./dashboard');
+
+// pour mock
+// simule la réponse de la function findOneByPk du
+// datamapper pour éviter d appeler la bdd durant les test
+// si on change l'id, ça passe plus
+// ça remplace la function quand elle est appelé par une des fonction testés
+jest.mock('../../datamappers/user',() => {
+  const originalModule = jest.requireActual('../../datamappers/user');
+  return {
+    ...originalModule,
+    findOneByPk : jest.fn(()=> ({
+      "id": 1,
+      "email": "marie.curie@mail.fr",
+      "pseudonym": "Marie",
+      "avatar_img": null,
+      "home_id": 1
+    }))
+  }
+})
+
 
 describe('dashboardController', () => {
   test('getNumberUsersinHome(user) home_id 1', async () => {
